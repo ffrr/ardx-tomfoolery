5df6815cf30a0fa868deb2949daeb5b141193919
František Karika <f@faux.sk>
2016-01-23 01:01:14 +0100

    cleaned up, added commentary, refactored

--- led-loop/led-loop.ino
+++ led-loop/led-loop.ino
@@ -3,114 +3,56 @@
  *     |  CIRC-02 .: 8 LED Fun :. (Multiple LEDs)   |
  *     ---------------------------------------------------------
  *  
- *  A few Simple LED animations
+ *  A pimped-up, configurable, knight rider version of the default ardx led animation.
  *
- * For more information on this circuit http://tinyurl.com/d2hrud
  *
  */
- 
-//LED Pin Variables
-int ledPins[] = {2,3,4,5,6,7,8,9}; //An array to hold the pin each LED is connected to
-                                   //i.e. LED #0 is connected to pin 2, LED #1, 3 and so on
-                                   //to address an array use ledPins[0] this would equal 2
-                                   //and ledPins[7] would equal 9
- 
-/*
- * setup() - this function runs once when you turn your Arduino on
- * We the three control pins to outputs
- */
-void setup()
-{
+
+
+int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9}; 
+                                                                      
+                                   
+void setup() {
   
   //Set each pin connected to an LED to output mode (pulling high (on) or low (off)
-  for(int i = 0; i < 8; i++){         //this is a loop and will repeat eight times
-      pinMode(ledPins[i],OUTPUT); //we use this to set each LED pin to output
-  }                                   //the code this replaces is below
+  for(int i = 0; i < 8; i++){   
+      pinMode(ledPins[i],OUTPUT); 
+  }                                  
 
 }
  
- 
-/*
- * loop() - this function will start after setup finishes and then repeat
- * we call a function called oneAfterAnother(). if you would like a different behaviour
- * uncomment (delete the two slashes) one of the other lines
- */
-void loop()                     // run over and over again
-{
-  int tailLen = 8;
-  int ledPinAmount = 8;
-  int delayTime = 500;
-  int cycleLen = ledPinAmount + tailLen;
 
-  // there and back again, in one cycle
+void loop() 
+{
+  int tailLen = 8; // length of the led 'tail'
+  int ledPinAmount = sizeof(ledPins); // length of the pin array 
+  int delayBetweenCycles = 500; // in ms
+  int delayBetweenDirectionChanges = 100;
+  int delayBetweenIterations = 20; // in ms
+  int cycleLen = ledPinAmount + tailLen; // length of the 'forward' part of the cycle
+
   for(int i = 0; i <= cycleLen * 2; i++) {
 
-    bool dir = i < cycleLen;
+    bool dir = i < cycleLen; // true - forward, false - backward
 
-    if(i == cycleLen) delay(100);
+    if(i == cycleLen) delay(delayBetweenDirectionChanges); // wait between direction changes
     
-    int idx = dir ? i:cycleLen * 2 - tailLen - i;
-    int dimIdx = idx + (!dir ? tailLen:-tailLen);
+    int lightIdx = dir ? i:cycleLen * 2 - tailLen - i; // the led index to light up
+    int dimIdx = lightIdx + (!dir ? tailLen:-tailLen); // the led index to dim
     
-    bool tailFilled = dir ? idx + 1 > tailLen : ledPinAmount - idx > tailLen;    
-    bool inLedRange = idx < ledPinAmount && idx >= 0;
+    // if the tail if filled, dim the last LED
+    bool tailFilled = dir ? lightIdx + 1 > tailLen : ledPinAmount - lightIdx > tailLen;    
     
-    if(inLedRange) digitalWrite(ledPins[idx], HIGH);    
+    // if a LED comes into range, light it up
+    bool inLedRange = lightIdx < ledPinAmount && lightIdx >= 0;
+    
+    if(inLedRange) digitalWrite(ledPins[lightIdx], HIGH);    
     if(tailFilled) digitalWrite(ledPins[dimIdx], LOW);
     
-    delay(20);
+    delay(delayBetweenIterations);
 
   }
 
-  delay(500);
-
-
+  delay(delayBetweenCycles);
 }
  
-/*
-void inAndOut(){
-  int delayTime = 100; //the time (in milliseconds) to pause between LEDs
-                       //make smaller for quicker switching and larger for slower
-  
-  //runs the LEDs out from the middle
-  for(int i = 0; i <= 3; i++){
-    int offLED = i - 1;  //Calculate which LED was turned on last time through
-    if(i == 0) {         //for i = 1 to 7 this is i minus 1 (i.e. if i = 2 we will
-      offLED = 3;        //turn on LED 2 and off LED 1)
-    }                    //however if i = 0 we don't want to turn of led -1 (doesn't exist)
-                         //instead we turn off LED 7, (looping around)
-    int onLED1 = 3 - i;       //this is the first LED to go on ie. LED #3 when i = 0 and LED 
-                             //#0 when i = 3 
-    int onLED2 = 4 + i;       //this is the first LED to go on ie. LED #4 when i = 0 and LED 
-                             //#7 when i = 3 
-    int offLED1 = 3 - offLED; //turns off the LED we turned on last time
-    int offLED2 = 4 + offLED; //turns off the LED we turned on last time
-    
-    digitalWrite(ledPins[onLED1], HIGH);
-    digitalWrite(ledPins[onLED2], HIGH);    
-    digitalWrite(ledPins[offLED1], LOW);    
-    digitalWrite(ledPins[offLED2], LOW);        
-    delay(delayTime);
-  }
- 
-  //runs the LEDs into the middle
-  for(int i = 3; i >= 0; i--){
-    int offLED = i + 1;  //Calculate which LED was turned on last time through
-    if(i == 3) {         //for i = 1 to 7 this is i minus 1 (i.e. if i = 2 we will
-      offLED = 0;        //turn on LED 2 and off LED 1)
-    }                    //however if i = 0 we don't want to turn of led -1 (doesn't exist)
-                         //instead we turn off LED 7, (looping around)
-    int onLED1 = 3 - i;       //this is the first LED to go on ie. LED #3 when i = 0 and LED 
-                             //#0 when i = 3 
-    int onLED2 = 4 + i;       //this is the first LED to go on ie. LED #4 when i = 0 and LED 
-                             //#7 when i = 3 
-    int offLED1 = 3 - offLED; //turns off the LED we turned on last time
-    int offLED2 = 4 + offLED; //turns off the LED we turned on last time
-    
-    digitalWrite(ledPins[onLED1], HIGH);
-    digitalWrite(ledPins[onLED2], HIGH);    
-    digitalWrite(ledPins[offLED1], LOW);    
-    digitalWrite(ledPins[offLED2], LOW);        
-    delay(delayTime);
-  }
-}*/
